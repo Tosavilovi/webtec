0b9e61db403be3e7b273aaa24d204b4d9e8c8456
Dimitrius <tosavilovi@mail.ru>
2019-10-07 23:10:20 +0300

    all

--- init.sh
+++ init.sh
@@ -1,6 +1,29 @@
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+echo 'Creating directories'
+
+mkdir -p web
+
+cd web && mkdir -p uploads public etc
+
+cd public && mkdir -p js css img && cd ..
+
+echo 'Create config files for nginx and unicorn'
+
+touch etc/nginx.conf && touch etc/gunicorn.conf
+
+echo 'Create links, remove default config from /etc/nginx/sites-enabled/default'
+
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+
+sudo rm /etc/nginx/sites-enabled/default 2> /dev/null
+
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+
 sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start﻿
+
+sudo /etc/init.d/mysql start
+
+echo 'Done'
+
+exit
